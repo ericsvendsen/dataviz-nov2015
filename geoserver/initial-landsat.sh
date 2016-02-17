4410ec5a8926bcff49c0a8bb8fc4a83837e22062
ericsvendsen <esvendsen@gmail.com>
2016-02-17 10:52:27 -0500

    modifying image processing

--- geoserver/initial-landsat.sh
+++ geoserver/initial-landsat.sh
@@ -16,7 +16,7 @@ do
     wget 'http://landsat-pds.s3.amazonaws.com/L8/027/039/LC80270392015'"${d}"'LGN00/LC80270392015'"${d}"'LGN00_B3.TIF'
     wget 'http://landsat-pds.s3.amazonaws.com/L8/027/039/LC80270392015'"${d}"'LGN00/LC80270392015'"${d}"'LGN00_B4.TIF'
     convert -combine LC80270392015"${d}"LGN00_B4.TIF LC80270392015"${d}"LGN00_B3.TIF LC80270392015"${d}"LGN00_B2.TIF LC80270392015"${d}"LGN00_RGB.TIF
-    convert -channel B -gamma 0.925 -channel R -gamma 1.03 -channel RGB -sigmoidal-contrast 50x16% LC80270392015"${d}"LGN00_RGB.TIF LC80270392015"${d}"LGN00_RGB_CORRECTED.TIF
+    convert -sigmoidal-contrast 50x16% LC80270392015"${d}"LGN00_RGB.TIF LC80270392015"${d}"LGN00_RGB_CORRECTED.TIF
     convert -depth 8 LC80270392015"${d}"LGN00_RGB_CORRECTED.TIF LC80270392015"${d}"LGN00_RGB_CORRECTED_8bit.TIF
     listgeo -tfw LC80270392015"${d}"LGN00_B4.TIF
     mv LC80270392015"${d}"LGN00_B4.tfw LC80270392015"${d}"LGN00_RGB_CORRECTED_8bit.tfw
