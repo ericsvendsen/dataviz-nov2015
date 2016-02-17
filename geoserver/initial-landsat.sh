78f72910836da466127003495d1e4550f73c127d
ericsvendsen <esvendsen@gmail.com>
2016-02-17 13:35:13 -0500

    adjusting color values

--- geoserver/initial-landsat.sh
+++ geoserver/initial-landsat.sh
@@ -7,39 +7,31 @@ mkdir -p ${DATA_DIR}
 mkdir -p ${CONFIG_DIR}
 cd ${DATA_DIR}
 
-#declare -a arrDays=('025' '073' '105' '121' '153' '201' '217' '249' '281' '313' '345')
-declare -a arrDays=('025' '073')
+declare -a arrDays=('025' '073' '105' '121' '153' '201' '217' '249' '281' '313' '345')
 
 for d in ${arrDays[@]}
 do
+    # Retrieve RGB landsat bands
     wget 'http://landsat-pds.s3.amazonaws.com/L8/027/039/LC80270392015'"${d}"'LGN00/LC80270392015'"${d}"'LGN00_B2.TIF'
     wget 'http://landsat-pds.s3.amazonaws.com/L8/027/039/LC80270392015'"${d}"'LGN00/LC80270392015'"${d}"'LGN00_B3.TIF'
     wget 'http://landsat-pds.s3.amazonaws.com/L8/027/039/LC80270392015'"${d}"'LGN00/LC80270392015'"${d}"'LGN00_B4.TIF'
+    # Reproject images
     gdalwarp -t_srs EPSG:3857 LC80270392015"${d}"LGN00_B2.TIF LC80270392015"${d}"LGN00_B2_PROJECTED.TIF
     gdalwarp -t_srs EPSG:3857 LC80270392015"${d}"LGN00_B3.TIF LC80270392015"${d}"LGN00_B3_PROJECTED.TIF
     gdalwarp -t_srs EPSG:3857 LC80270392015"${d}"LGN00_B4.TIF LC80270392015"${d}"LGN00_B4_PROJECTED.TIF
+    # Combine bands into one RGB image
     convert -combine LC80270392015"${d}"LGN00_B4_PROJECTED.TIF LC80270392015"${d}"LGN00_B3_PROJECTED.TIF LC80270392015"${d}"LGN00_B2_PROJECTED.TIF LC80270392015"${d}"LGN00_RGB.TIF
-    convert -channel B -gamma 0.925 -channel R -gamma 1.03 -channel RGB -sigmoidal-contrast 50x16% LC80270392015"${d}"LGN00_RGB.TIF LC80270392015"${d}"LGN00_RGB_CORRECTED.TIF
+    # Adjust image color
+    convert -channel B -gamma 1.1 -channel R -gamma 0.9 -channel RGB -sigmoidal-contrast 50x22% LC80270392015"${d}"LGN00_RGB.TIF LC80270392015"${d}"LGN00_RGB_CORRECTED.TIF
+    # Convert to 8 bit image
     convert -depth 8 LC80270392015"${d}"LGN00_RGB_CORRECTED.TIF LC80270392015"${d}"LGN00_RGB_CORRECTED_8bit.TIF
+    # Retrieve geo data from base image and reapply it to the corrected 8-bit RGB image
     listgeo -tfw LC80270392015"${d}"LGN00_B4_PROJECTED.TIF
     mv LC80270392015"${d}"LGN00_B4_PROJECTED.tfw LC80270392015"${d}"LGN00_RGB_CORRECTED_8bit.tfw
     gdal_edit.py -a_srs EPSG:3857 LC80270392015"${d}"LGN00_RGB_CORRECTED_8bit.TIF
-    ls | grep -v _8bit.TIF | xargs rm
-    #wget 'https://s3.amazonaws.com/ais-landsat/LC80270392015'"${d}"'LGN00.tar.gz'
-    #tar -xzvf 'LC80270392015'"${d}"'LGN00.tar.gz'
-    #rm 'LC80270392015'"${d}"'LGN00.tar.gz'
-    #gdal_merge.py -separate -o LC80270392015025LGN00_BV.TIF LC80270392015025LGN00_B4.TIF LC80270392015025LGN00_B3.TIF LC80270392015025LGN00_B2.TIF
     # Toss all input files
-    #ls | grep -v _BV.TIF | xargs rm
+    ls | grep -v _8bit.TIF | xargs rm
 done
-
-# Create inner tiles and overviews to ensure snappy rendering performance
-#for FILE in `ls *.TIF`; do
-#    echo ${FILE}
-#    gdal_translate -co "TILED=YES" -co "BLOCKXSIZE=512" -co "BLOCKYSIZE=512" ${FILE} tiled_${FILE}
-#    gdaladdo -r average tiled_${FILE} 2 4 8 16 32
-#    mv tiled_${FILE} ${FILE}
-#done
 
 # Copy in the mosaic configuration
 cp /tmp/geoserver/landsat/* ${CONFIG_DIR}
