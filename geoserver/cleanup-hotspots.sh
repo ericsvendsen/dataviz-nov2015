f314a41ea91a0204bb14a31a68e7471a4d0dcbd0
ericsvendsen <esvendsen@gmail.com>
2016-03-09 10:46:19 -0500

    commented out removal of workspace for hotspots cleanup

--- geoserver/cleanup-hotspots.sh
+++ geoserver/cleanup-hotspots.sh
@@ -3,7 +3,7 @@
 HOTSPOTS_DIR='/srv/geoserver/mosaics/hotspots'
 
 # Remove the entire mosaic workspace
-curl -v -u admin:geoserver -XDELETE http://localhost:8080/geoserver/rest/workspaces/mosaic?recurse=true
+#curl -v -u admin:geoserver -XDELETE http://localhost:8080/geoserver/rest/workspaces/mosaic?recurse=true
 
 # Remove all generated garbage from the mosaic configuration directory
 rm -fr ${HOTSPOTS_DIR}
